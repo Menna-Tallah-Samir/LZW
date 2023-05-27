e26fc5e67999d748f7685d758d28c71cfa34241a
blu-ray <yasminehassan2621@gmail.com>
2023-05-27 23:18:39 +0300

    added jar

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,11 +2,14 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        if(args[0]=="compress"){
+        System.out.println("Hello");
+        if(args[0].equals("compress")){
+            System.out.println("Starting compressing");
             CompressionLZW compress = new CompressionLZW();
             compress.compressFile(args[1]);
         }
-        else if(args[0]=="decompress"){
+        else if(args[0].equals("decompress")){
+            System.out.println("Starting decompressing");
             DecompressionLZW decompressionLZW = new DecompressionLZW();
             decompressionLZW.decompressFile(args[1]);
         }
